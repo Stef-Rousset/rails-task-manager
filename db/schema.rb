f19bfd92aef591ad127af770e21cafebbc9308f7
stephanie rousset <strabon@club-internet.fr>
2020-08-11 15:32:18 +0200

    completed exo

--- db/schema.rb
+++ db/schema.rb
@@ -19,5 +19,4 @@ ActiveRecord::Schema.define(version: 2020_08_11_094238) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
